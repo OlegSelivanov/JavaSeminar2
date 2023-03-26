3e4b8a857d047dfcc3eafde25d7e577245c4c31f
Oleg Selivanov <alien3569@mail.ru>
2023-03-26 14:01:16 +0300

    Add Task3

---
+++ src/lesson2/Main.java
@@ -0,0 +1,16 @@
+package lesson2;
+
+import task.Task1;
+import task.Task2;
+import task.Task3;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.printf("%s", Task1.getRepeatString(6));
+//        System.out.println(Task2.stringCompression("aaaabbbcdda"));
+        Task3.write("TEST");
+    }
+}
